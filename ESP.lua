b98763c8a15a34dc90c2f080ff5a8aa1a398f53c
oskar4000 <oskar.h.vainionpaa@gmail.com>
2025-05-17 02:17:30 +0300

    ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -1,9 +1,4 @@
-return function(Rayfield, Window)
-    local MainTab = Window:CreateTab("💢Main💢", nil)
-    
-    ----------------
-    -- ESP SECTION --
-    ----------------
+return function(Rayfield, Window, MainTab)
     local ESPSection = MainTab:CreateSection("ESP")
 
     -- ESP Variables
@@ -14,43 +9,24 @@ return function(Rayfield, Window)
     local espColor = Color3.fromRGB(170, 0, 255)
     local teamEspColor = Color3.fromRGB(0, 170, 255)
 
-    -- ... [keep all your existing ESP functions here] ...
-
-    -- ESP Toggle Button
-    local EspButton = MainTab:CreateButton({
-       Name = "ESP Toggle",
-       Callback = function()
-          -- ... [keep your existing callback] ...
-       end
-    })
-
-    -- Team ESP Toggle Button
-    local TeamEspButton = MainTab:CreateButton({
-       Name = "Team ESP Toggle",
-       Callback = function()
-          -- ... [keep your existing callback] ...
-       end
-    })
-
-    -- ESP Color Picker
-    local EspColorPicker = MainTab:CreateColorPicker({
-        Name = "ESP Color",
-        Color = espColor,
-        Flag = "ESPColorPicker",
-        Callback = function(Value)
-            espColor = Value
-            updateHighlightColors()
+    -- [Include all your ESP functions here...]
+    
+    -- ESP Toggle
+    ESPSection:CreateButton({
+        Name = "ESP Toggle",
+        Callback = function()
+            espEnabled = not espEnabled
+            -- [Your ESP toggle logic...]
         end
     })
 
-    -- Team ESP Color Picker
-    local TeamEspColorPicker = MainTab:CreateColorPicker({
-        Name = "Team ESP Color",
-        Color = teamEspColor,
-        Flag = "TeamESPColorPicker",
+    -- ESP Color Picker
+    ESPSection:CreateColorPicker({
+        Name = "ESP Color",
+        Color = espColor,
         Callback = function(Value)
-            teamEspColor = Value
-            updateHighlightColors()
+            espColor = Value
+            -- [Update highlights...]
         end
     })
 end
